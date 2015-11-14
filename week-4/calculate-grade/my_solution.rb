77587d5f6f85e824704e34c6650fa0d9928cd543
Alex Mitzman <amitzman10@gmail.com>
2015-11-13 20:25:03 -0500

    Solve pairing challenges

---
+++ week-4/calculate-grade/my_solution.rb
@@ -0,0 +1,22 @@
+# Calculate a Grade
+
+# I worked on this challenge [by myself and with:Albert Hahn ].
+
+
+# Your Solution Below
+
+def get_grade(avg)
+  if avg >= 90
+  return "A"
+  elsif avg >= 80
+  return "B"
+  elsif avg >= 70
+  return "C"
+  elsif avg >= 60
+  return "D"
+  else
+  return "F"
+  end
+end
+
+p get_grade(75)
---
+++ week-4/leap-years/my_solution.rb
@@ -0,0 +1,17 @@
+# Leap Years
+
+# I worked on this challenge [by myself, with: Albert Hahn ].
+
+
+# Your Solution Below
+def leap_year?(leap)
+  if leap % 4 != 0
+  return false
+  elsif leap % 4 == 0 && leap % 100 == 0 && leap % 400 != 0
+  return false
+  else
+  return true
+  end
+end
+
+p leap_year?(2004)
