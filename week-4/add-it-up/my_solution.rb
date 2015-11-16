afb71da0f8cf92d1898716448c12dfad077e43b8
Alex Mitzman <amitzman10@gmail.com>
2015-11-16 00:38:23 -0500

    Fix indentation

--- week-4/add-it-up/my_solution.rb
+++ week-4/add-it-up/my_solution.rb
@@ -4,7 +4,7 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.
 
-# I worked on this challenge [by myself, with: Saundra].
+# I worked on this challenge [by myself, with: Saundra Castaneda].
 
 # 0. total Pseudocode
 # make sure all pseudocode is commented out!
