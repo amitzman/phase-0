98804c9fad72548badca4226ae911dc5d0e2c23f
Alex Mitzman <amitzman10@gmail.com>
2015-11-23 00:02:28 -0500

    Add reflection

--- week-5/groups/acct_groups.rb
+++ week-5/groups/acct_groups.rb
@@ -40,5 +40,25 @@ end
 acct_groups('don', 'gary', 'kaleb', 'victor', 'howard', 'joel', 'danny', 'david', 'mike', 'chris', 'carter', 'sammy', 'becca', 'rachel', 'sunny', 'marie', 'jodi', 'morgan', 'amanda', 'bobby', 'buck', 'reggie', 'carl')
 
 
+# REFLECTION
 
+# What was the most interesting and most difficult part of this challenge?
+
+  # The most difficult problem for me was to make sure that there was minimum of three students in each group and moving students from another group to the other.
+
+# Do you feel you are improving in your ability to write pseudocode and break the problem down?
+
+  # I do think I am doing a better job at being able to break down the problem.  Although I believe I could use some work with the language of how I write it.
+
+# Was your approach for automating this task a good solution? What could have made it even better?
+
+  # I believe I had a solid solution for this task, but it could be a lot better.  For instance, if there were eight people to be split up into a group, it would be split up with five people in one group and three in the other group.  Ideally, would like to have each group with four people instead.
+
+# What data structure did you decide to store the accountability groups in and why?
+
+  # I used arrays with sub-arrays to store the information mainly because I’m more familiar with them.  I didn’t think it would be too hard to access the information, because each group was just an element.  If I wanted to find which group a particular person was in then a hash probably would have been the way to go.
+
+# What did you learn in the process of refactoring your initial solution? Did you learn any new Ruby methods?
+
+  # A new method that I applied for the first time was .each_slice.  This allows a user to slice up an array, by a number of elements.  So if I did .each_slice(4) on an array of 10 elements, it would make two smaller arrays (slices) of four elements and one array (slice) of the remaining two elements.  Another one I used was .shuffle on an array which randomized the elements in the array.
 
--- week-5/nums-commas/my_solution.rb
+++ week-5/nums-commas/my_solution.rb
@@ -63,3 +63,24 @@ puts separate_comma(10000000000000000)
 
 
 # 3. Reflection
+
+# What was your process for breaking the problem down? What different approaches did you consider?
+
+  # My process was to define an input and what my desired result to be.  Then from there I did some pretty general steps to take to get the end result.  Then went back through each step to add more detail to how I was going to achieve that step.
+
+
+# Was your pseudocode effective in helping you build a successful initial solution?
+
+  # Yeah, it helped for an initial solution and helped pass the tests.  However, my initial solution wouldn’t have worked for much larger numbers, like numbers that needed more than three commas.
+
+# What new Ruby method(s) did you use when refactoring your solution? Describe your experience of using the Ruby documentation to implement it/them (any difficulties, etc.). Did it/they significantly change the way your code works? If so, how?
+
+  # New method I used was the .ceil which is short for ceiling.  What this this does is round a float up to the nearest integer.  I used this to help me determine how many commas are necessary.  This changed it significantly because it finally helped me get a better way of getting the number of commas.  At first I had a long series of IF and ELSIF statements for determining the number of commas and this method made the code much more cleaner and shorter.
+
+# How did you initially iterate through the data structure?
+
+  # I used the .times iterator to go through the array to add commas every [-4] element.  I researched and tried using the .each_slice method.  I came close to successfully using it, but couldn’t quite get there.
+
+# Do you feel your refactored solution is more readable than your initial solution? Why?
+
+  # I made it a little more readable.  Was able to create a variable which helped clean up .times piece of code.
